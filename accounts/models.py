bb29f8ebcad4b057fcdadfd5659137395e92950d
olamide <olamoyegunolamidesammy@gmail.com>
2025-01-19 01:19:10 +0100

    Initial commit for Real Estate SaaS project

---
+++ accounts/models.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.contrib.auth import get_user_model
+
+class Audit(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class CustomUser(AbstractUser, Audit):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+class UserProfile(Audit):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    location = models.CharField(max_length=255, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.email} Profile'
+    
+class UserRole(Audit):
+    USER_ROLES = (
+        ('Admin', 'Admin'),
+        ('Agent', 'Agent'),
+        ('Customer', 'Customer'),
+    )
+
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    role = models.CharField(max_length=50, choices=USER_ROLES)
+
+    def __str__(self):
+        return f'{self.user.email} - {self.role}'
