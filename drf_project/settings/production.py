ac26d07043fa21c37b83be9daecfc75d2ed24e36
olamide <olamoyegunolamidesammy@gmail.com>
2025-04-05 18:21:54 +0100

    fix logging error on render again

--- drf_project/settings/production.py
+++ drf_project/settings/production.py
@@ -6,17 +6,12 @@ LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "handlers": {
-        "file": {
-            "level": "ERROR",
-            "class": "logging.FileHandler",
-            "filename": "logs/django_errors.log",
+        "console": {
+            "class": "logging.StreamHandler",
         },
     },
-    "loggers": {
-        "django": {
-            "handlers": ["file"],
-            "level": "ERROR",
-            "propagate": True,
-        },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
     },
 }
